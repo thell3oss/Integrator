38233a1238ae72d7fe888a80eeffea1c8c84bff6
Student <student@piomba154016.local>
2016-05-11 09:32:02 -0400

    REWORD ME. Fix bug in Simpson's Rule/Make integration calculation simpler and easier to follow. Maybe fixed loss of precision.

--- src/integrator/SimpsonsRule.java
+++ src/integrator/SimpsonsRule.java
@@ -16,29 +16,32 @@ public class SimpsonsRule extends GenericIntegrator {
 		}
     }
 	public double specificSum(double x, Function function, double deltaX) {
-		return (    function.evaluate(x) +
-				2 * function.evaluate(x + deltaX) + 
-				    function.evaluate(x + 2 * deltaX)) * deltaX / 3;
+		return function.evaluate(x) + 4 * function.evaluate(x + deltaX) + function.evaluate(x + 2 * deltaX)
 	}
 	
 	@Override
 	public double Integrate(double start, double end, int numberOfIntervals, Function function) {
-		if (numberOfIntervals % 2 == 1) {
-			// Is this a good idea?
+                /* Simpson’s rule is (f(x_0) + 4 * f(x_1) + f(x_2)) * deltaX / 6
+                 * Where x_0, x_1, and x_2 are all equally spaced from each other
+                 * (x_0 + deltaX = x_1)
+                 * If you want more slices, then you can repeat Simpson’s rule at
+                 * x_2 -> x_4, x_4 -> x_6, and so on.
+                 * Resulting in (f(x_0) + 4 * f(x_1) + 2f(x_2) + 4f(x_3) + 2f(x_4) . . . 
+                 * + 2f(x_n-2) + 4f(x_n-1) + f(x_n)) * deltaX / 6
+                 */
+
+                if (numberOfIntervals % 2 == 1) 
+                        // Is this a good idea?
 			throw new RuntimeException("Simpson's Rule requires that the number of intervals be even.");			
 		}
 		
 		double deltaX = (end - start)/numberOfIntervals;
 		double total = 0;
-		//x_0 and x_n
-		total += function.evaluate(start) + function.evaluate(end);
-		//x_1 through x_n-1
-		for (double x = start + deltaX; x < end - deltaX; x += 2 * deltaX) {
-			// 4*f(x_1) + 2*f(x_2) + 4*f(x_3) + 2*f(x_4)...
-			total += 4* function.evaluate(x) +
-					2* function.evaluate(x + deltaX);
+		for (double x = start; x < end; x += deltaX) {
+			total += specificSum(x, function, deltaX);
 		}
-		// Remember that deltaX / 3 encloses the whole thing.
-		return total * deltaX / 3;
+                // TODO: Investigate loss of precision stuff.
+                // Do deltaX / 6 here because it reduces loss of precision
+		return total * deltaX / 6;
 	}
 }
