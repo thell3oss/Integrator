1904649bf6c281809714e56afe3c2aaed2f05c12
Aaron Kofsky <a2aarontothe2@gmail.com>
2016-05-14 18:23:25 -0400

    Fix alignment of text fields.
    This commit aligns the fields vertically, rather than horizontally.

--- src/integrator/GUI.java
+++ src/integrator/GUI.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Group;
 import javax.swing.GroupLayout.ParallelGroup;
 import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JComponent;
@@ -17,56 +18,79 @@ import javax.swing.JTextPane;
 
 public class GUI extends JFrame{
 	public GUI() {
-		 setTitle("Integrator");
-		 setSize(600, 500);
-		 setLocationRelativeTo(null);
-		 setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setTitle("Integrator");
+		setSize(600, 500);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		 //Set up container + groups
-		 Container pane = getContentPane();
+		//Set up container + groups
+		Container pane = getContentPane();
 
-		 GroupLayout layout = new GroupLayout(pane);
-		 pane.setLayout(layout);        	        
+		GroupLayout layout = new GroupLayout(pane);
+		pane.setLayout(layout);        	        
 
-		 SequentialGroup hGroup = layout.createSequentialGroup();        
-		 ParallelGroup vGroup = layout.createParallelGroup();
-		 
-		 // Create labels which ask for the function and interval to use.
-		 JPanel function_label = NewLabel("What function?");
-		 JPanel interval_label = NewLabel("What interval?");
-		 JPanel subinterval_label = NewLabel("How many subintervals?");
-		 
-		 hGroup
-		 	.addComponent(function_label)
-		 	.addComponent(interval_label)
-		 	.addComponent(subinterval_label);
-		 vGroup
-		 	.addComponent(function_label)
-		 	.addComponent(interval_label)
-		 	.addComponent(subinterval_label);
-		 
-		 // Finally, set the layout to have the groups we have.
-		 layout.setHorizontalGroup(hGroup);
-		 layout.setVerticalGroup(vGroup);
-	}
-	
-	private JPanel NewLabel(String labelName) {
-	     JPanel panel = new JPanel(); 
+		SequentialGroup hGroup = layout.createSequentialGroup();        
+		ParallelGroup vGroup = layout.createParallelGroup();
 		
-	     JTextField textPane = new JTextField();
-	     textPane.setPreferredSize(new Dimension(70, 20));
-	     
-	     
-	     JLabel label = new JLabel(labelName);
-	     
-	     label.setLabelFor(textPane);
-	     Font serif = new Font("Serif", Font.PLAIN, 14);
-	     label.setFont(serif);
-	     label.setForeground(new Color(50, 50, 25));
-	     
-	     panel.add(textPane);
-	     panel.add(label);
-	     
-	     return panel;
-	}   
+		// Create text areas which ask for the function and interval to use.
+		JLabel function_label = new JLabel("What function?");
+		JTextField function_field = new JTextField(16);
+		
+		JLabel interval_label = new JLabel("What interval? (Format is [a,b])");
+		JTextField interval_field = new JTextField(4);
+		
+		JLabel subinterval_label = new JLabel("How many subintervals?");
+		JTextField subinterval_field = new JTextField(4);
+		
+		
+		// Add the text areas
+		hGroup.addGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(function_label)
+						.addComponent(interval_label)
+						.addComponent(subinterval_label))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(function_field, 100, 150, 200)
+						.addComponent(interval_field, 40, 60, 80)
+						.addComponent(subinterval_field, 16, 20, 30))
+				);
+		
+		vGroup.addGroup(layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(function_label)
+						.addComponent(function_field))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(interval_label)
+						.addComponent(interval_field))
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(subinterval_label)
+						.addComponent(subinterval_field))
+				);
+		// Finally, set the layout to have the groups we have.
+		layout.setHorizontalGroup(hGroup);
+		layout.setVerticalGroup(vGroup);
+
+
+	}
+
+	private JPanel NewLabel(String labelName, int width, int height) {
+		JPanel panel = new JPanel(); 
+
+		JTextField textPane = new JTextField();
+		textPane.setPreferredSize(new Dimension(width, height));
+
+
+		JLabel label = new JLabel(labelName);
+
+		label.setLabelFor(textPane);
+		Font serif = new Font("Serif", Font.PLAIN, 14);
+		label.setFont(serif);
+		label.setForeground(new Color(50, 50, 25));
+
+		panel.add(label);
+		panel.add(textPane);
+
+
+		return panel;
+	}
 }
