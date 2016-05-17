267bf27405b5d3c904f8187806b1aaef237ea379
thell3oss <ealluse@gmail.com>
2016-05-17 10:43:07 -0400

    Add files via upload
    Message boxes now pop up when Simpson's Rule is attempted when the initial or final number of subintervals is odd. Message boxes also pop up when integration is attempted with a negative amount of subintervals or when a function isn't given.

---
+++ Function.java
@@ -0,0 +1,85 @@
+import org.nfunk.jep.JEP;
+/**
+ * Write a description of class Function here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Function
+{
+    private String function;
+    private JEP parser;
+    
+    public Function(String function)
+    {
+        this.function = function;
+        
+        parser = new JEP();
+        parser.addStandardFunctions();
+        parser.addStandardConstants();
+    }
+    
+    public double evaluate(double x)
+    {
+        parser.addVariable("x", x);
+        parser.parseExpression(function);
+        return parser.getValue();
+    }
+    
+    
+    public double LRS(double start, double end, int numberOfIntervals)
+    {
+        double deltaX = (end - start) / numberOfIntervals;
+        double total = 0;
+        for (double x = start; x < end; x += deltaX) {
+            total += this.evaluate(x) * deltaX;
+        }
+        return total;
+    }
+    
+    public double MRS(double start, double end, int numberOfIntervals)
+    {
+        double deltaX = (end - start) / numberOfIntervals;
+        return LRS(start + (deltaX / 2), end, numberOfIntervals);
+    }
+    
+    public double RRS(double start, double end, int numberOfIntervals)
+    {
+        double deltaX = (end - start) / numberOfIntervals;
+        return LRS(start + deltaX, end, numberOfIntervals);
+    }
+    
+    public double trapezoidalSum(double start, double end, int numberOfIntervals)
+    {
+        double deltaX = (end - start) / numberOfIntervals;
+        double total = 0;
+        for (double x = start; x < end; x += deltaX)
+        {
+            total += (this.evaluate(x) + this.evaluate(x + deltaX)) * (deltaX / 2);
+        }
+        return total;
+    }
+    
+   public double simpsonsRule(double start, double end, int numberOfIntervals)
+    {
+        if (numberOfIntervals % 2 == 1)
+        {
+            throw new IllegalArgumentException("Simpsons's Rule does not accept an odd number of intervals!");
+        }
+        
+        double deltaX = (end - start) / numberOfIntervals;
+        double total = 0;
+        
+        int i = 0;
+        for (double x = start; x < end; x += 2 * deltaX, i ++) //x is incremented by 2 because the right endpoint of the middlemost parabolas is the left endoint of other middle parabolas
+        {
+            double xStar = x;
+            double left = this.evaluate(xStar);
+            double mid = this.evaluate(xStar + deltaX);
+            double right = this.evaluate(xStar + 2 * deltaX);
+            total += left + (4 * mid) + right;
+        }
+        
+        return total * (deltaX / 3);
+    }
+}
---
+++ GUI.java
@@ -0,0 +1,148 @@
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+import BreezySwing.DoubleField;
+import BreezySwing.GBFrame;
+import BreezySwing.IntegerField;
+
+public class GUI extends GBFrame
+{
+    private JTextField fxnInput = addTextField("", 1, 2, 1, 1);
+    private JTextField lowerLimitInput = addTextField("", 2, 2, 1, 1);
+    private JTextField upperLimitInput = addTextField("", 3, 2, 1, 1);
+    private IntegerField initNumberOfIntervals = addIntegerField(1, 4, 2, 1, 1);
+    private IntegerField finalNumberOfIntervals = addIntegerField(2, 5, 2, 1, 1);
+    
+    private ButtonGroup approximationSelector = new ButtonGroup();
+    private JRadioButton lrs = addRadioButton("Left Riemann Sum", 1, 3, 1, 1);
+    private JRadioButton mrs = addRadioButton("Middle Riemann Sum", 2, 3, 1, 1);
+    private JRadioButton rrs = addRadioButton("Right Riemann Sum", 3, 3, 1, 1);
+    private JRadioButton tRule = addRadioButton("Trapezoidal Rule", 4, 3, 1, 1);
+    private JRadioButton sRule = addRadioButton("Simpson's Rule", 5, 3, 1, 1);
+    
+    private JButton calculate = addButton("CALCULATE", 6, 1, 1, 1);
+    
+    private JLabel loadingIndicator = addLabel("", 7, 1, 1, 1);
+    private JList table = addList(8, 1, 1, 1);
+    
+    public GUI()
+    {
+        setTitle("Integrator");
+        
+        addLabel("Enter the function to be integrated: ", 1, 1, 1, 1);
+        addLabel("lower limit: ", 2, 1, 1, 1);
+        addLabel("upper limit: ", 3, 1, 1, 1);
+        addLabel("initial amount of intervals: ", 4, 1, 1, 1);
+        addLabel("final amount of intervals: ", 5, 1, 1, 1);
+        
+        approximationSelector.add(lrs);
+        approximationSelector.add(mrs);
+        approximationSelector.add(rrs);
+        approximationSelector.add(tRule);
+        approximationSelector.add(sRule);
+        
+        setSize(600, 600);
+        setVisible(true);
+    }
+    
+    private void respond()
+    {
+        Function fx = new Function(fxnInput.getText());
+        int n1 = initNumberOfIntervals.getNumber(), n2 = finalNumberOfIntervals.getNumber();
+        double a = new Function(lowerLimitInput.getText()).evaluate(1), b = new Function(upperLimitInput.getText()).evaluate(1);
+        
+        if (fx.equals(""))
+        {
+            showMessage("You must enter a function.");
+        }
+        
+        if (n1 < 0)
+        {
+            showMessage("The initial amount of subintervals cannot be negative.");
+            return;
+        }
+        if (n2 < 0)
+        {
+            showMessage("The final amount of subintervals cannot be negative.");
+            return;
+        }
+        
+        DefaultListModel<String> tableContents = new DefaultListModel<String>();
+        if (lrs.isSelected())
+        {
+            for (int i = n1; i <= n2; i ++)
+            {
+                tableContents.addElement(i + "\t\t|" + fx.LRS(a, b, i));
+            }
+        }
+        else if (mrs.isSelected())
+        {
+            for (int i = n1; i <= n2; i ++)
+            {
+                tableContents.addElement(i + "\t\t|" + fx.MRS(a, b, i));
+            }
+        }
+        else if (rrs.isSelected())
+        {
+            for (int i = n1; i <= n2; i ++)
+            {
+                tableContents.addElement(i + "\t\t|" + fx.RRS(a, b, i));
+            }
+        }
+        else if (tRule.isSelected())
+        {
+            for (int i = n1; i <= n2; i ++)
+            {
+                tableContents.addElement(i + "\t\t|" + fx.trapezoidalSum(a, b, i));
+            }
+        }
+        else if (sRule.isSelected())
+        {
+            if (n1 % 2 == 1)
+            {
+               showMessage("The inital amount of subintervals cannot be odd when using Simpson's Rule!");
+               return;
+            }
+            if (n2 % 2 == 1)
+            {
+               showMessage("The final amount of subintervals cannot be odd when using Simpson's Rule!");
+               return;
+            }
+            for (int i = n1; i <= n2; i ++)
+            {
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+                tableContents.addElement(i + "\t\t|" + fx.simpsonsRule(a, b, i));
+            }
+        }
+        else
+        {
+            showMessage("You must select an approximation method.");
+        }
+        table.setModel(tableContents);  
+        
+        revalidate();
+        repaint();
+    }
+    
+    private void showMessage(String text)
+    {
+        new JOptionPane().showMessageDialog(this, text);
+    }
+    
+    public void buttonClicked(JButton b)
+    {
+        if (b == calculate)
+        {
+            respond();
+        }
+    }
+}
